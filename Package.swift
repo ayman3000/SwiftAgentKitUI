ade10c3b35cd8385114bc9d601187bc8442e0277
Ayman Hamed <ayman3000@gmail.com>
2026-07-06 14:02:07 +0300

    chore: bump package stack to alpha 5

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SwiftAgentKitUI", targets: ["SwiftAgentKitUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ayman3000/SwiftAgentKit.git", from: "0.1.0-alpha.4"),
-        .package(url: "https://github.com/ayman3000/LLMProviderKit.git", from: "0.1.0-alpha.4"),
+        .package(url: "https://github.com/ayman3000/SwiftAgentKit.git", from: "0.1.0-alpha.5"),
+        .package(url: "https://github.com/ayman3000/LLMProviderKit.git", from: "0.1.0-alpha.5"),
     ],
     targets: [
         .target(
